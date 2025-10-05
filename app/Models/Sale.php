3937110e9d3de20feaae81cc16865e4be3fe2f4c
Muazam Mughal <104276759+MuazamMughal@users.noreply.github.com>
2025-10-06 03:50:21 +0500

    started work on salesitem/sales model initial integration done

--- app/Models/Sale.php
+++ app/Models/Sale.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use App\Models\SaleItem;
 use Illuminate\Database\Eloquent\Model;
 
 class Sale extends Model
 {
-    //
+    protected $fillable = [
+        'invoice_no',
+        'total',
+        'tax',
+        'discount',
+        'grand_total',
+        'payment_method',
+    ];
+    public function items() {
+        return $this->hasMany(SaleItem::class);
+    }
 }
