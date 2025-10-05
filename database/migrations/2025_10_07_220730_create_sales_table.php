fa6cbd0f41f0a2b9f684253799d4d3102d060573
Muazam Mughal <104276759+MuazamMughal@users.noreply.github.com>
2025-10-05 18:20:33 -0400

    started work on sales/ table done

--- database/migrations/2025_10_07_220730_create_sales_table.php
+++ database/migrations/2025_10_07_220730_create_sales_table.php
@@ -13,7 +13,14 @@ return new class extends Migration
     {
         Schema::create('sales', function (Blueprint $table) {
             $table->id();
+            $table->string('invoice_no')->unique();
+            $table->decimal('total', 10, 2);
+            $table->decimal('tax', 10, 2)->default(0);
+            $table->decimal('discount', 10, 2)->default(0);
+            $table->decimal('grand_total', 10, 2);
+            $table->string('payment_method')->default('cash'); // cash, card, wallet
             $table->timestamps();
+        
         });
     }
 
