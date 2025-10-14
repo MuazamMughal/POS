5eef01bdbf8a3b83994ffa16c1bd247c119b5b1e
Muazam Mughal <104276759+MuazamMughal@users.noreply.github.com>
2025-10-14 08:01:26 +0500

    started working on purchaseitem

--- app/Models/PurchaseItem.php
+++ app/Models/PurchaseItem.php
@@ -3,8 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Purchase;
+use App\Models\Product;
 
 class PurchaseItem extends Model
 {
-    //
+    protected $fillable = [
+        'purchase_id',
+        'product_id',
+        'quantity',
+        'cost',
+        'subtotal',
+    ];
+
+    public function purchase()
+    {
+        return $this->belongsTo(Purchase::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
