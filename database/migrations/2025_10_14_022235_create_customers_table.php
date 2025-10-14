433a2a6ccc39c2cab3c98645a7c3cfdef72951a5
Muazam Mughal <104276759+MuazamMughal@users.noreply.github.com>
2025-10-14 07:28:12 +0500

    started working on customer

--- database/migrations/2025_10_14_022235_create_customers_table.php
+++ database/migrations/2025_10_14_022235_create_customers_table.php
@@ -12,8 +12,14 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('customers', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+           $table->id();
+           $table->string('name');
+           $table->string('phone')->unique();
+           $table->string('email')->nullable();
+           $table->decimal('loyalty_points', 10, 2)->default(0);
+           $table->decimal('credit_limit', 10, 2)->default(0);
+           $table->decimal('balance_due', 10, 2)->default(0);
+           $table->timestamps();
         });
     }
 
