8f23a6fc06018cdc43d94a40c299398754a6bdb9
Muazam Mughal <104276759+MuazamMughal@users.noreply.github.com>
2025-10-22 11:42:23 +0500

    Routes changes

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ use App\Http\Controllers\DashboardHome;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SaleController;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\PurchaseController;
+use App\Http\Controllers\SupplierController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -45,6 +48,43 @@ Route::middleware('auth')->group(function () {
 Route::middleware('auth')->group(function () {
     Route::get('/pos', [SaleController::class, 'index'])->name('pos.index');
     Route::post('/sales', [SaleController::class, 'store'])->name('sales.store');
+});
+
+//routes for customers
+Route::middleware('auth')->group(function () {
+    Route::get('/customers', [CustomerController::class, 'index'])->name('customers.index');
+    Route::get('/customers/create', [CustomerController::class, 'create'])->name('customers.create');
+    Route::post('/customers/store', [CustomerController::class, 'store'])->name('customers.store');
+    Route::delete('/customers/{customer}', [CustomerController::class, 'destroy'])->name('customers.destroy');  
+    Route::get('/customers/{customer}/edit', [CustomerController::class, 'edit'])->name('customers.edit');
+    Route::put('/customers/update/{customer}', [CustomerController::class, 'update'])->name('customers.update');
+});
+
+//routes for sales
+Route::middleware('auth')->group(function () {
+    Route::get('/sales', [SaleController::class, 'Sales'])->name('sales.index');
+    Route::get('/sales/{sale}', [SaleController::class, 'show'])->name('sales.show');
+    Route::delete('/sales/{sale}', [SaleController::class, 'destroy'])->name('sales.destroy');
+});
+
+//routes for suppliers
+Route::middleware('auth')->group(function () {
+    Route::get('/suppliers', [SupplierController::class, 'index'])->name('suppliers.index');
+    Route::get('/suppliers/create', [SupplierController::class, 'create'])->name('suppliers.create');
+    Route::post('/suppliers/store', [SupplierController::class, 'store'])->name('suppliers.store');
+    Route::delete('/suppliers/{supplier}', [SupplierController::class, 'destroy'])->name('suppliers.destroy');  
+    Route::get('/suppliers/{supplier}/edit', [SupplierController::class, 'edit'])->name('suppliers.edit');
+    Route::put('/suppliers/update/{supplier}', [SupplierController::class, 'update'])->name('suppliers.update');
+});
+
+//routes for purchases
+Route::middleware('auth')->group(function () {
+    Route::get('/purchases', [PurchaseController::class, 'index'])->name('purchases.index');
+    Route::get('/purchases/create', [PurchaseController::class, 'create'])->name('purchases.create');
+    Route::post('/purchases/store', [PurchaseController::class, 'store'])->name('purchases.store');
+    Route::get('/purchases/{purchase}/show', [PurchaseController::class, 'Show'])->name('purchases.edit');
+    Route::put('/purchases/update/{purchase}', [PurchaseController::class, 'update'])->name('purchases.update');
+    Route::delete('/purchases/{purchase}', [PurchaseController::class, 'destroy'])->name('purchases.destroy');
 });
 
 require __DIR__.'/auth.php';
